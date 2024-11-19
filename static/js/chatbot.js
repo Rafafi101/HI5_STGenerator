0c4e8d62fb54b95ef48a7c7bc959527773df89f4
Rafafi123 <gonzalesrafael93@gmail.com>
2024-11-19 12:48:13 +0800

    Generating User response for user messages

---
+++ static/js/chatbot.js
@@ -0,0 +1,62 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+// Insert API KEY here
+const API_KEY = "";
+
+const createChatLi = (message, className) => {
+    //Create a chat<li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
+// Ivan! dito mo lagay sagot mo sa pagammahal
+const generateResponse = (incomingChatLi) => {
+    // Insert API here
+    const API_URL = "";
+    const messageElement = incomingChatLi.querySelector("p")
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
+
+    // Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    })
+
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() => {
+        //Display "Thinking ..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        generateResponse();
+    }, 600);
+}
+
+
+sendChatBtn.addEventListener("click", handleChat);
+
